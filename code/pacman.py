8e4b3c2e0d1e43c283158068f46acc9bcb62c17f
Elias Nijs <elias.nijs@gmail.com>
2022-10-18 15:13:08 +0200

    updated todo.txt, found bug

--- code/pacman.py
+++ code/pacman.py
@@ -36,8 +36,29 @@ def kb_upsingle(button:Button):
 # =======================================================
 # NOTE(Elias): Helper Functions
 
-def isEmpty(row: int, col: int, game: Game) -> bool:
+def is_empty(row: int, col: int, game: Game) -> bool:
     return game.tiles[row][col] in [Tiles.EMPTY.value, Tiles.PELLET.value, Tiles.POWER.value]
+
+def rand_dir() -> vec2:
+    return DIRS[r.randint(0, 3)]
+
+# NOTE(Bavo): call this at beginning and end of ghost movement. Returns if the ghost may still move
+def handle_touch(game: Game, ghost: Ghost) -> bool:
+    if ghost.body.pos == game.pacman.pos:
+        if not ghost.killable:
+            game.running = False
+        else:
+            ghost.body.pos = ghost.spawn
+            ghost.body.dir = vec2(0, 1)
+            ghost.deathtime = GHOSTRESPAWN
+            ghost.killable = False
+            game.score += game.combo
+            game.combo *= 2
+        return False
+    return True
+
+# =======================================================
+# NOTE(Elias): Game
 
 # TODO(Elias): Do this in a better way... color already set on the map?
 # NOTE(Bavo): colors of the ghosts, and thus also the maximum number of ghosts
@@ -71,28 +92,6 @@ def loadmap(path: str) -> tuple[list[list[str]], Body, list[Body]]:
     if pacman is None:
         raise Exception("No pacman (symbol = 'P') found on the map")
     return tiles, pacman, ghosts
-
-
-def rand_dir() -> vec2:
-    return DIRS[r.randint(0, 3)]
-
-# NOTE(Bavo): call this at beginning and end of ghost movement. Returns if the ghost may still move
-def handle_touch(game: Game, ghost: Ghost) -> bool:
-    if ghost.body.pos == game.pacman.pos:
-        if not ghost.killable:
-            game.running = False
-        else:
-            ghost.body.pos = ghost.spawn
-            ghost.body.dir = vec2(0, 1)
-            ghost.deathtime = GHOSTRESPAWN
-            ghost.killable = False
-            game.score += game.combo
-            game.combo *= 2
-        return False
-    return True
-
-# =======================================================
-# NOTE(Elias): Game
     
 def game_update(game:Game) -> None:
 
@@ -123,7 +122,7 @@ def game_update(game:Game) -> None:
         game.combo = 0
 
     pn = game.pacman.pos + game.pacman.dir
-    if 0 <= pn.x < game.w and 0 <= pn.y < game.h and isEmpty(pn.y, pn.x, game):
+    if 0 <= pn.x < game.w and 0 <= pn.y < game.h and is_empty(pn.y, pn.x, game):
         game.pacman.pos = pn
         if game.tiles[pn.y][pn.x] == Tiles.PELLET.value:
             game.tiles[pn.y][pn.x] = Tiles.EMPTY.value
@@ -148,7 +147,7 @@ def game_update(game:Game) -> None:
                 open = []
                 for dir in DIRS:
                     pn = ghost.body.pos + dir
-                    if (0 <= pn.x < game.w and 0 <= pn.y < game.h and isEmpty(pn.y, pn.x, game)
+                    if (0 <= pn.x < game.w and 0 <= pn.y < game.h and is_empty(pn.y, pn.x, game)
                             and dir != vec2(- ghost.body.dir.x, - ghost.body.dir.y)):
                         open.append(dir)
                 spots = len(open)
@@ -158,7 +157,7 @@ def game_update(game:Game) -> None:
                     ghost.body.dir = r.choice(open)
 
                 pn = ghost.body.pos + ghost.body.dir
-                if 0 <= pn.x < game.w and 0 <= pn.y < game.h and isEmpty(pn.y, pn.x, game):
+                if 0 <= pn.x < game.w and 0 <= pn.y < game.h and is_empty(pn.y, pn.x, game):
                     ghost.body.pos = pn
 
                 handle_touch(game, ghost)
